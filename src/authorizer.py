3a697337a26a3295912f3d9385646a81377a984f
jakub-figat <jakub.figat@neontri.com>
2022-10-23 17:40:02 +0200

    Add powertools to authorizer

--- src/authorizer.py
+++ src/authorizer.py
@@ -1,9 +1,15 @@
 from typing import Any
 
+from aws_lambda_powertools.logging import Logger
+
 from src.services import TokenService
 from src.services.token import TokenServiceException
 
 
+logger = Logger()
+
+
+@logger.inject_lambda_context(log_event=True)
 def authorize_from_jwt(event: dict[str, Any], context: Any) -> dict[str, Any]:
     authorization_header = event["headers"].get("Authorization")
     if authorization_header is None:
@@ -13,5 +19,6 @@ def authorize_from_jwt(event: dict[str, Any], context: Any) -> dict[str, Any]:
         token = TokenService.parse_token_from_header(header=authorization_header)
         token_data = TokenService.decode_token(token=token)
         return {"isAuthorized": True, "context": {"user": token_data["sub"]}}
-    except TokenServiceException:
+    except TokenServiceException as exc:
+        logger.warning("TokenException: %s" % str(exc))
         return {"isAuthorized": False}
